1cf22a32f982476e9c8be38da40d95f01a15e03a
Junior <marm45123@gmail.com>
2020-11-08 17:10:46 +0200

    do WhileAdded

--- LetsGo.cpp
+++ LetsGo.cpp
@@ -12,50 +12,62 @@
 using namespace std;
 int main()
 {
-    string name, department;
-    int username, degree;
-    cout << "Enter your name: ";
-    cin >> name;
-    cout << "Enter your usrename: ";
-    cin >> username;
-    cout << "Enter your department A or T or C: ";
-    cin >> department;
-    cout << "Enter your degree: ";
-    cin >> degree;
-    cout << "Name: " << name << endl;
-    cout << "Username: " << username << endl;
-    if (department == "A")
-    {
-        cout << "Department: Control" << endl;
-    }
-    if (department == "T")
-    {
-        cout << "Department: Communication" << endl;
-    }
-    if (department == "C")
-    {
-        cout << "Department: Computer" << endl;
-    }
-    switch (degree)
-    {
-    case 0 ... 59:
-        cout << "Degree: F";
-        break;
-    case 60 ... 69:
-        cout << "Degree: D";
-        break;
-    case 70 ... 79:
-        cout << "Degree: C";
-        break;
-    case 80 ... 89:
-        cout << "Degree: B";
-        break;
-    case 90 ... 100:
-        cout << "Degree: A";
-        break;
-    default:
-        cout << "Out Of Range!" << endl;
-    }
+    string name, department; 
+    char Exit;
+    int usernameID, degree;
 
+    do
+    {
+        cout << "Enter your name: ";
+        cin >> name;
+        cout << "Enter your usrename: ";
+        cin >> usernameID;
+        cout << "Enter your department A or T or C: ";
+        cin >> department;
+        cout << "Enter your degree: ";
+        cin >> degree;
+       
+        
+        cout << "Name: " << name << endl;
+        cout << "Username: " << usernameID << endl;
+        if (department == "A")
+        {
+            cout << "Department: Control" << endl;
+        }
+        if (department == "T")
+        {
+            cout << "Department: Communication" << endl;
+        }
+        if (department == "C")
+        {
+            cout << "Department: Computer" << endl;
+        }
+        switch (degree)
+        {
+        case 0 ... 59:
+            cout << "Degree: F" << endl;
+            break;
+        case 60 ... 69:
+            cout << "Degree: D" << endl;
+            break;
+        case 70 ... 79:
+            cout << "Degree: C" << endl;
+            break;
+        case 80 ... 89:
+            cout << "Degree: B" << endl;
+            break;
+        case 90 ... 100:
+            cout << "Degree: A" << endl;
+            break;
+        default:
+            cout << "Out Of Range!" << endl;
+        
+        
+        }
+        cout << "Do you want Exit [Y or N]";
+        cin >> Exit;
+    } while (Exit != 'y');
+    
+    
     return 0;
 }
